d2d4b4bf398c12ede4fe21774646829b29a6f78f
aaa01452 <kelly@omniway.com.tw>
2024-10-28 17:34:25 +0800

    feat: add echo.

--- jenkins/image_version.sh
+++ jenkins/image_version.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "Get image version"
+
 
 response=$(curl -s -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${DOCKERHUB_CREDENTIALS}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/users/aaa01452/packages/container/myapp_test/versions?per_page=1")
+
+echo "check:  $response"
 
 # 使用 grep 和 sed 解析 JSON 並取得第一個 tag
 tag_value=$(echo "$response" | grep -o '"tags":\s*\[\s*"[^"]*' | sed 's/.*"//')
