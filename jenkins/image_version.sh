abbb251e2a1c6d181ff62dea07740c5f3e88f671
aaa01452 <kelly@omniway.com.tw>
2024-10-28 17:38:20 +0800

    feat: add echo.

--- jenkins/image_version.sh
+++ jenkins/image_version.sh
@@ -8,8 +8,6 @@ response=$(curl -s -L \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/users/aaa01452/packages/container/myapp_test/versions?per_page=1")
 
-echo "check:  $response"
-
 # 使用 grep 和 sed 解析 JSON 並取得第一個 tag
 tag_value=$(echo "$response" | sed -n 's/.*"tags":\s*\[\s*"\([^"]*\)".*/\1/p')
 
