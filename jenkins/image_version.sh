1414c6ee6a1800052db5262bf8777a8f2fc2fd88
aaa01452 <kelly@omniway.com.tw>
2024-10-28 17:29:52 +0800

    feat: update file.

--- jenkins/image_version.sh
+++ jenkins/image_version.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# 使用 curl 呼叫 API 並將回傳值儲存在變數中
 response=$(curl -s -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${DOCKERHUB_CREDENTIALS}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/users/aaa01452/packages/container/myapp_test/versions?per_page=1")
 
-# 使用 jq 解析 tags 的第一筆數值
-tag_value=$(echo "$response" | jq -r '.[0].metadata.container.tags[0]')
+# 使用 grep 和 sed 解析 JSON 並取得第一個 tag
+tag_value=$(echo "$response" | grep -o '"tags":\s*\[\s*"[^"]*' | sed 's/.*"//')
 
-# 將數值輸出
 echo "$tag_value"
